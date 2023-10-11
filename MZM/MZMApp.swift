b89b8dc2f5a229fcfe33bc915de2debdc47e669e
iOS-Yetti <trentkim95@gmail.com>
2023-10-11 16:42:54 +0900

    git test

---
+++ MZM/MZMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MZMApp.swift
+//  MZM
+//
+//  Created by 예찬 on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MZMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
